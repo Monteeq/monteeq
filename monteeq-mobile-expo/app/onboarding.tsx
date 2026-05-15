9c81abe0d8f458f4813eea1d755b912b6a8d26bd
Sadiqu Muhammad Bello <sadiqulmasduq2010@gmail.com>
2026-05-15 23:58:39 +0100

    Delete monteeq-mobile-expo directory

--- monteeq-mobile-expo/app/onboarding.tsx
+++
@@ -1,7 +0,0 @@
-import { OnboardingScreen } from '@/screens/OnboardingScreen';
-import { useRouter } from 'expo-router';
-
-export default function OnboardingRoute() {
-  const router = useRouter();
-  return <OnboardingScreen onFinish={() => router.replace('/(tabs)')} />;
-}
