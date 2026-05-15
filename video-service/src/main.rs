583a2cbecb6d2eb3b9efae49c67b6e123cc40eb0
Smasduq <sadiqulmasduq2010@gmail.com>
2026-05-16 00:26:01 +0100

    added port to rust

--- video-service/src/main.rs
+++ video-service/src/main.rs
@@ -78,7 +78,11 @@ async fn main() {
         .layer(CorsLayer::permissive())
         .with_state(state);
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 8081));
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "8081".to_string())
+        .parse()
+        .expect("PORT must be a valid number");
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     println!("Monteeq High-Performance Video Service listening on {}", addr);
     
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
