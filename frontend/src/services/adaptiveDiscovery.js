571166f7b6b4c819ac132d74cd9f66cf9219b14b
Smasduq <sadiqulmasduq2010@gmail.com>
2026-08-05 23:41:39 +0100

    chore: remove deprecated Vite frontend (superseded by Next.js app)

--- frontend/src/services/adaptiveDiscovery.js
+++
@@ -1,7 +0,0 @@
-export const adaptiveDiscovery = {
-    reRankBatch: (videos, layerResponse) => videos,
-    recordImpression: () => {},
-    recordEngagement: () => {},
-    recordSkip: (id, category) => ({ tier: 0 }),
-    recordWatch: (videoId, watchMs, durationMs, mood) => {},
-};
